6bc6ac646a1b1363443834ce16535e52561d1689
Shane <shane@peregrinum.dev>
2025-07-16 17:42:47 +0200

    [any message]

--- app/Main.hs
+++ app/Main.hs
@@ -35,10 +35,10 @@ main = do
         "decode" -> do
             -- You can use print statements as follows for debugging, they'll be visible when running tests.
             hPutStrLn stderr "Logs from your program will appear here!"
-            -- Uncomment this block to pass stage 1
-            -- let encodedValue = args !! 1
-            -- let decodedValue = decodeBencodedValue(B.pack encodedValue)
-            -- let jsonValue = encode(B.unpack decodedValue)
-            -- LB.putStr jsonValue
-            -- putStr "\n"
+            Uncomment this block to pass stage 1
+            let encodedValue = args !! 1
+            let decodedValue = decodeBencodedValue(B.pack encodedValue)
+            let jsonValue = encode(B.unpack decodedValue)
+            LB.putStr jsonValue
+            putStr "\n"
         _ -> putStrLn $ "Unknown command: " ++ command
